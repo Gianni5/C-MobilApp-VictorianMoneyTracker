ac76ebe02357dbab4122636daf1b9344912c5741
Gianni5 <69453802+Gianni5@users.noreply.github.com>
2022-03-09 15:56:44 +0800

    first commit Victorian Money Tracker

---
+++ VictorianMoneyTracker2/MainPage.xaml.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VictorianMoneyTracker2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        money Money = new money(); //  object for money class 
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+
+
+
+        }
+
+        //*************************************** POUND *******************************************
+        private void PlusPound_Clicked(object sender, EventArgs e)
+        {
+
+            Money.poundPlus();
+            updateUI();
+
+        }
+
+        private void poundDown_Clicked(object sender, EventArgs e)//********** POUND DOWN *****************
+        {
+
+            Money.poundConvertDown();
+            updateUI();
+
+        }
+
+        private void MinuesPound_Clicked(object sender, EventArgs e)
+        {
+
+            Money.poundMinus();
+            updateUI();
+        }
+       
+
+        //************************************ CROWN **********************************************
+
+        private void PlusCrown_Clicked(object sender, EventArgs e) 
+        {
+
+            Money.crownPlus();
+            updateUI();
+        }
+
+        private void crownUp_Clicked(object sender, EventArgs e)//***************** CROWN UP ***********
+        {
+
+
+            Money.crownsConvertUp();
+            updateUI();
+
+        }
+
+        private void crownDown_Clicked(object sender, EventArgs e)//***************** CROWN DOWN *********
+        {
+
+            Money.crownConvertDown();
+            updateUI();
+
+        }
+
+        private void MinuesCrown_Clicked(object sender, EventArgs e)
+        {
+
+            Money.crownMinus();
+            updateUI();
+        }
+
+       
+
+
+        //****************************************** SHILLING ****************************************
+
+        private void PlusShilling_Clicked(object sender, EventArgs e)
+        {
+
+            Money.shillingPlus();
+            updateUI();
+        }
+
+        private void shillUp_Clicked(object sender, EventArgs e)//******** SHILLING UP ***************
+        {
+            Money.shillingConvertUp();
+            updateUI();
+        }
+
+        private void shillDown_Clicked(object sender, EventArgs e)//********************* SHILLING DOWN *********
+        {
+
+            Money.shillingConvertDown();
+            updateUI();
+
+
+        }
+
+        private void MinuesShilling_Clicked(object sender, EventArgs e)
+        {
+
+            Money.shillingMinus();
+            updateUI();
+        }
+
+
+        //************************************* PENCE *********************************************
+
+        private void PlusPence_Clicked(object sender, EventArgs e)
+        {
+
+            Money.pencePlus();
+            updateUI(); //butt
+            
+            // link xamarin to c#
+            penceNum.Text = Money.pence.ToString();            
+        }
+
+        private void penceUp_Clicked(object sender, EventArgs e)// **************** PENCE UP *******************
+        {
+            Money.penceConvertUp();
+            updateUI();
+            penceNum.Text = Money.pence.ToString();
+        }
+
+
+
+        private void penceDown_Clicked(object sender, EventArgs e)// **************** PENCE DOWN **************
+        {
+            Money.penceConvertDown();
+            updateUI();
+
+        
+        }
+
+        private void MinuesPence_Clicked(object sender, EventArgs e)
+        {
+
+            Money.penceMinus();
+            updateUI();
+            
+        }
+
+        //********************************* FARTHING **************************************
+        public void PlusFarthingBottonClick(object sender, EventArgs e)
+        {
+
+            Money.farthingPlus();
+            updateUI();
+        }
+        private void farthUp_Clicked(object sender, EventArgs e)// ********************** FARTHING UP **********
+        {
+
+            Money.farthingConvertUp();
+            updateUI();
+
+        }
+
+        private void MinuesFarth_Clicked(object sender, EventArgs e)
+        {
+
+            Money.farthingMinus();
+            updateUI();
+
+        }
+
+
+
+
+        public void updateUI()
+        {
+            //******************************* POUND ************************************
+            poundNum.Text = Money.pound.ToString();  // link xamarin to c#
+
+            if (Money.pound == 0)
+            {
+                MinuesPound.IsEnabled = false;
+                poundDown.IsEnabled = false;
+                poundDown.Source = "downArrowDisabled.png";
+            }
+            else
+            {
+                MinuesPound.IsEnabled = true;
+                poundDown.IsEnabled = true;
+                poundDown.Source = "downArrowEnabled.png";
+            }
+
+            //************************** CROWN **************************************
+            crownNum.Text = Money.crown.ToString();   // link xamarin to c#
+
+            if (Money.crown < Money.crownToPoundConvert)
+            {
+                crownUp.IsEnabled = false;
+                crownUp.Source = "upArrowDisabled.png";
+
+            }
+            else 
+            {
+                crownUp.IsEnabled = true;
+                crownUp.Source = "upArrowEnabled.png";
+            }
+
+            if (Money.crown == 0)
+            {
+                MinuesCrown.IsEnabled = false;
+                crownDown.IsEnabled = false;
+                crownDown.Source = "downArrowDisabled.png";
+            }
+            else 
+            {
+                MinuesCrown.IsEnabled = true;
+                crownDown.IsEnabled = true;
+                crownDown.Source = "downArrowEnabled.png";
+            }
+
+
+
+            //****************************** SHILLING ******************************
+            shillNum.Text = Money.shilling.ToString();   // link xamarin to c#
+
+            if (Money.shilling < Money.shillingToCrownConvert)
+            {
+                shillUp.IsEnabled = false;
+                shillUp.Source = "upArrowDisabled.png";
+            }
+            else
+            {
+                shillUp.IsEnabled = true;
+                shillUp.Source = "upArrowEnabled.png";
+            }
+
+            if (Money.shilling == 0)
+            {
+                MinuesShilling.IsEnabled = false;
+                shillDown.IsEnabled = false;
+                shillDown.Source = "downArrowDisabled.png";
+            }
+            else
+            {
+                MinuesShilling.IsEnabled = true;
+                shillDown.IsEnabled = true;
+                shillDown.Source = "downArrowEnabled.png";
+            }
+
+
+            //**************************** PENCE ******************************************* 
+            penceNum.Text = Money.pence.ToString();   // link xamarin to c#
+
+            if (Money.pence < Money.penceToShillingConvert)
+            {
+                penceUp.IsEnabled = false;
+                penceUp.Source = "upArrowDisabled.png";
+            }
+            else
+            {
+                penceUp.IsEnabled = true;
+                penceUp.Source = "upArrowEnabled.png";
+            }
+            if (Money.pence == 0)
+            {
+                MinuesPence.IsEnabled = false;
+                penceDown.IsEnabled = false;
+                penceDown.Source = "downArrowDisabled.png";
+            }
+            else
+            {
+                MinuesPence.IsEnabled = true;
+                penceDown.IsEnabled = true;
+                penceDown.Source = "downArrowEnabled.png";
+            }
+
+
+
+            //**************************** FARTHING **********************************
+            farthNum.Text = Money.farthing.ToString();  // link xamarin to c#
+            
+            if (Money.farthing < Money.farthingToPenceConvert)
+            {
+                farthUp.IsEnabled = false;
+                farthUp.Source = "upArrowDisable.png";
+            }
+            else
+            {
+                farthUp.IsEnabled = true;
+                farthUp.Source = "upArrowEnable.png";
+            }
+
+            if (Money.farthing == 0)
+            {
+                MinuesFarth.IsEnabled = false;
+            }
+            else 
+            {
+                MinuesFarth.IsEnabled = true;
+            }
+
+
+        }
+    }
+}
---
+++ VictorianMoneyTracker2/money.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VictorianMoneyTracker2
+{
+    public class money
+    {
+        int total = 0;
+        public int pound { get; set; } = 0;
+        public int crown { get; set; } = 0;
+        public int shilling { get; set; } = 0;
+        public int pence { get; set; } = 0;
+        public int farthing { get; set; } = 0;
+
+
+        // this is my convertion rate
+        public int poundToCrownConvert { get; set; } = 4;
+        public int crownToShilingConvert { get; set; } = 5;
+        public int shillingToPeneConvert { get; set; } = 12;
+        public int penceToFarthingConvert { get; set; } = 4;
+
+        public int crownToPoundConvert { get; set; } = 4;
+        public int shillingToCrownConvert { get; set; } = 5;
+        public int penceToShillingConvert { get; set; } = 12;
+        public int farthingToPenceConvert { get; set; } = 4;
+
+        //******************************************** CONVERT DOWN FUNCTIONS ************************
+        public void poundConvertDown() //convert pound to crown 
+        {
+
+            pound--;
+            crown = crown + poundToCrownConvert;
+
+
+
+        }
+
+        public void crownConvertDown() // convert crown to shilling 
+        {
+            crown--;
+            shilling = shilling + crownToShilingConvert;
+        }
+
+        public void shillingConvertDown() // convert shilling to pence
+        {
+            shilling--;
+            pence = pence + shillingToPeneConvert;
+
+        }
+
+        public void penceConvertDown() // convert pence to farthing 
+        {
+            pence--;
+            farthing = farthing + penceToFarthingConvert;
+
+
+        }
+
+        //************************************ CONVERT UP FUNCTIONS **********************************
+
+        public void crownsConvertUp() // convert crown to pound 
+        {
+
+            crown = crown - crownToPoundConvert;
+            pound++;
+
+
+        }
+
+        public void shillingConvertUp() // convert shilling to crown
+        {
+            shilling = shilling - shillingToCrownConvert;
+            crown++;
+
+        }
+
+        public void penceConvertUp() // convert pence to shilling 
+        {
+            pence = pence - penceToShillingConvert;
+            shilling++;
+
+        }
+
+        public void farthingConvertUp() // convert farthing to pence 
+        {
+            farthing = farthing - farthingToPenceConvert;
+            pence++;
+        }
+
+        //*************************** MINUS BUTTONS ************************************************
+
+        public void farthingMinus()
+        {
+            farthing--;
+            
+        }
+
+        public void penceMinus()
+        {
+            pence--;
+        }
+
+        public void shillingMinus()
+        {
+           
+            shilling--;
+        }
+
+        public void crownMinus()
+        {
+            crown--;
+        }
+
+        public void poundMinus()
+        {
+            pound--;
+        }
+
+        //******************************* PLUS BUTTONS *************************************
+
+        public void farthingPlus()
+        {
+            farthing++;
+            //pence--;
+        }
+
+        public void pencePlus()
+        {
+            pence++;
+        }
+
+        public void shillingPlus()
+        {
+            shilling++;
+        }
+
+        public void crownPlus()
+        {
+            crown++;
+        }
+
+        public void poundPlus()
+        {
+            pound++;
+        }
+    }
+}
